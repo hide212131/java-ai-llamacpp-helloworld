f64735946b81b977d332a74dcb1945d90413e033
Hideyuki Kagami <hide212131@gmail.com>
2023-12-29 23:50:03 +0900

    Initial commit

---
+++ src/main/java/com/example/aillamacpphelloworld/AiLlamacppHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.aillamacpphelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLlamacppHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLlamacppHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aillamacpphelloworld/AiLlamacppHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aillamacpphelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLlamacppHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
